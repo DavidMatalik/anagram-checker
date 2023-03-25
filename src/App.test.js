9b59acf24afd89ef705b588be20992759c729f65
David Matalik <david.matalik@posteo.de>
2023-03-25 13:12:56 +0100

    Fix failing test

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import App from './App'
 
-test('should check if two words are anagrams', () => {
+test('should check if two words are anagrams', async () => {
   render(<App />)
 
   const word1Input = screen.getByLabelText('Word 1:')
@@ -14,20 +14,22 @@ test('should check if two words are anagrams', () => {
   userEvent.click(checkButton)
 
   expect(
-    screen.getByText('Yeah success: The words are anagrams.')
+    await screen.findByText('Yeah success: The words are anagrams.')
   ).toBeInTheDocument()
 })
 
-test('should not check if one of the words is missing', () => {
+test('should check if the words are no anagrams', async () => {
   render(<App />)
 
   const word1Input = screen.getByLabelText('Word 1:')
+  const word2Input = screen.getByLabelText('Word 2:')
   const checkButton = screen.getByRole('button', { name: 'Check Anagram' })
 
   userEvent.type(word1Input, 'listen')
+  userEvent.type(word2Input, 'another')
   userEvent.click(checkButton)
 
   expect(
-    screen.queryByText('The words are not anagrams.')
-  ).not.toBeInTheDocument()
+    await screen.findByText('The words are not anagrams.')
+  ).toBeInTheDocument()
 })
